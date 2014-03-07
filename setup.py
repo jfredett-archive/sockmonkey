a391e87a25d7a20d52ac083f5370587b5b64983d
Joe Fredette <jfredett@gmail.com>
2014-03-07 00:04:04 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+        name='sockmonkey',
+        version='0.0.0',
+        author='Joe Fredette',
+        author_email='jfredett@gmail.com',
+        packages=['sockmonkey'],
+        scripts=[],
+        url='http://pypi.python.org/pypi/sockmonkey/',
+        license='LICENSE',
+        description='A tool for identifying potential sockpuppet reddit accounts',
+        long_description=open('README.md').read(),
+        install_requires=[
+            "nltk == 2.0.4",
+            "numpy == 1.8.0",
+            "pyyaml == 3.10"
+        ]
+)
